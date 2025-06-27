d1fa9beb55b6f125cb797de13495fcceb77a73a6
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2025-06-27 09:59:48 +0300

    SettingsView and converters...

---
+++ AzureSpeechProject/Converters/BoolToShowHideTextMultiConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Avalonia.Data.Converters;
+
+namespace AzureSpeechProject.Converters;
+
+public class BoolToShowHideTextMultiConverter : IMultiValueConverter
+{
+    public object Convert(IList<object> values, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (values?.FirstOrDefault() is bool showKey)
+        {
+            return showKey ? "🔓 Hide" : "🔒 Show";
+        }
+        return "🔒 Show";
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
